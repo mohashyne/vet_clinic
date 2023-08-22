90e534d0513c482b0509b79191f906cb09a523dc
MSA <crystalbluetech@gmail.com>
2023-08-22 14:00:17 +0100

    create table for animals

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
- CREATE TABLE animals (
-id INTEGER,
-name VARCHAR(300),
-date_of_birth DATE,
+CREATE TABLE animals (
+id BIGSERIAL NOT NULL PRIMARY KEY,
+name VARCHAR(300) NOT NULL,
+date_of_birth DATE NOT NULL,
 escape_attempts INTEGER,
 neutered BOOLEAN,
 weight_kg DECIMAL );
